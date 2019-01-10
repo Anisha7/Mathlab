f3856f2469b9ba6b57f39da4754a4096011b1820
Anisha Jain <anishajain@outlook.com>
2019-01-09 23:37:45 -0800

    api works!!

--- scripts.js
+++ scripts.js
@@ -12,21 +12,58 @@ function getRandomInt(max) {
 
 // use wolfram api to verify
 function verify(value) {
+    console.log("verifying")
     // URL for queries
-    URL = `http://www.wolframalpha.com/queryrecognizer/query.jsp?appid=L3KTPE-UYAAY8W3TG&mode=Default&i=${value}&output=json`
+    URL = `https://cors-escape.herokuapp.com/http://www.wolframalpha.com/queryrecognizer/query.jsp?appid=L3KTPE-UYAAY8W3TG&mode=Default&i=${value}&output=json`
 
     // Create a request variable and assign a new XMLHttpRequest object to it.
-    var request = new XMLHttpRequest();
+    // let request = new XMLHttpRequest();
 
-    // Open a new connection, using the GET request on the URL endpoint
-    request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+    // // Open a new connection, using the GET request on the URL endpoint
+    // request.open('GET', URL, true);
+    // console.log(request.status)
+    // request.onload = function () {
+    //     // Begin accessing JSON data here
+    //     console.log(this.response)
+    //     var data = JSON.parse(this.response);
+    //     console.log("JSON")
+    //     console.log(data)
+    // }
 
-    request.onload = function () {
-    // Begin accessing JSON data here
-    }
-
+    // request.onreadystatechange = function () {
+    //     // Begin accessing JSON data here
+    //     console.log(this.response)
+    //     var data = JSON.parse(this.response);
+    //     console.log("JSON")
+    //     console.log(data)
+    // }
+    
     // Send request
     // request.send();
 
+    // fetch(URL).then(function(result) {
+    //     console.log("Result: ")
+    //     console.log(result.json())
+    //     // return result.json();
+    //   }).then(function(json) {
+    //     // displayResults(json);
+    //     console.log("HI, display it now")
+    // });
+
+    axios({
+        method:'get',
+        url: URL,
+        responseType:'text'
+      })
+        .then(function(response) {
+        // response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+        console.log("Result: ")
+        console.log(response)
+        console.log(response.data)
+    });
     return true
 }
+
+console.log("TEST: ")
+verify("12 + 4")
+console.log("TEST END")
--- scripts/simple.js
+++ scripts/simple.js
@@ -6,14 +6,14 @@ console.log("scripts loaded")
 stuff = generateSimpleProblem()
 console.log(stuff)
 // from mdn documentation
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * Math.floor(max));
+// }
 
-// use wolfram api to verify
-function verify(equation) {
-    return true
-}
+// // use wolfram api to verify
+// function verify(equation) {
+//     return true
+// }
 
 // simple math problems
 function generateSimpleProblem(){
@@ -36,13 +36,11 @@ function generateSimpleProblem(){
 
 // add equation to html page
 function printSimpleProblem(){
-    console.log("I am trying")
     problem = generateSimpleProblem()
     document.getElementById('problem-string').innerText = `Solve ${problem}.`
 }
 
 window.onload = function() {
-    console.log(document.getElementById("simple"))
     printSimpleProblem()
     // check for click events
     document.getElementById("simple").onclick = function(e){
