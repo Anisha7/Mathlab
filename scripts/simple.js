812dfb479b87342be240caf81605941befb395bd
Anisha Jain <anishajain@outlook.com>
2019-01-12 16:45:21 -0800

    break code into helpers

--- scripts/simple.js
+++ scripts/simple.js
@@ -4,23 +4,29 @@
 // track problem to solution
 let solutionDict = {};
 
+// make simple add/subtract equation
+function makeEquation(){
+    let max = 50
+    let num1 = getRandomInt(max)
+    let num2 = getRandomInt(max)
+    // determine random operand
+    let opNum = getRandomInt(100)
+    let op = 'plus'
+    if (opNum%2 === 0) {
+        op = 'minus'
+    }
+    let equation = `${num1} ${op} ${num2}`
+    return equation
+}
+
 async function generateSimpleProblem(){
     console.log("STARTING")
-    let max = 50
     let found = false
     let count = 0
     let equation = '3 plus 5'
-    while (found !== true && count < 5){
+    while ((found !== true) && (count < 5)){
         console.log("**in while loop**")
-        let num1 = getRandomInt(max)
-        let num2 = getRandomInt(max)
-        // determine random operand
-        let opNum = getRandomInt(100)
-        let op = 'plus'
-        if (opNum%2 === 0) {
-            op = 'minus'
-        }
-        equation = `${num1} ${op} ${num2}`
+        equation = makeEquation()
         console.log(equation)
         // setTimeout(10000)
         found = await verify(equation)
